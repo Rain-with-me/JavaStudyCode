4c729f1f3c9a5800a2af711ebb631980dbcf3cdc
“Rain-with-me” <2168702791@qq.com>
2022-10-15 11:35:39 +0800

    微信登录

--- 4-springboot-security-dynic/src/main/java/com/lu/security/config/security/AdminUserDetails.java
+++ 4-springboot-security-dynic/src/main/java/com/lu/security/config/security/AdminUserDetails.java
@@ -8,7 +8,6 @@ import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.List;
---
+++ 6-springboot-weixin-main/springboot-weixin/src/test/java/com/crush/weixin/Test.java
@@ -0,0 +1,13 @@
+package com.crush.weixin;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @Author: 雨同我
+ * @Description:
+ * @DateTime: 2022/10/15 11:19
+ **/
+public class Test {
+
+}
